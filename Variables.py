8c0f9429859ec62bb3862c2eacfc72c1d9c5bd4d
Franco <franco.miguez98@gmail.com>
2024-06-08 15:15:12 -0300

    Clase Introducción a Python

---
+++ Variables.py
@@ -0,0 +1,15 @@
+"""
+    Tipos de datos
+    Int -> 123
+    float -> 3.14
+    boold -> True False
+    String -> "Hola"
+"""
+
+
+#Variables
+mi_variable = 15
+
+#Constantes
+MI_VARIABLE = "Hola"
+
