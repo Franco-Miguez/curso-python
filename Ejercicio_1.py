750d7708a67befc2ed9e0d66f712288ccf59198f
Franco <franco.miguez98@gmail.com>
2024-06-10 15:42:09 -0300

    Modificamos ejercicio 1 y realizamos el 2

--- Ejercicio_1.py
+++ Ejercicio_1.py
@@ -2,4 +2,5 @@ nombre = input("Ingrese su Nombre: ")
 apellido = input("Ingrese su Apellido: ")
 edad = input("Ingrese su Edad: ")
 
-print(nombre, apellido, "tiene", edad, "años")
+print(f"{nombre} {apellido} tiene {edad} años")
+#print(nombre, apellido, "tiene", edad, "años")
---
+++ Ejercicio_2.py
@@ -0,0 +1,3 @@
+nuevo_archivo = input("Ingrese nombre del archivo: ")
+
+print(f"C:\\Windows\\user\\{nuevo_archivo}")
