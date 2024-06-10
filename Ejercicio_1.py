244c55521de9db14ddb895c3f15ea793902da202
Franco <franco.miguez98@gmail.com>
2024-06-10 15:15:13 -0300

    Ejercicio 1 y 2 listo

---
+++ Ejercicio_1.py
@@ -0,0 +1,5 @@
+nombre = input("Ingrese su Nombre: ")
+apellido = input("Ingrese su Apellido: ")
+edad = input("Ingrese su Edad: ")
+
+print(nombre, apellido, "tiene", edad, "años")
---
+++ Ejercicio_3.py
@@ -0,0 +1,9 @@
+texto = input("Ingrese texto: ")
+boolean = input("Ingrese un valor boolean: ")
+integer = input("Ingrese un numero entero: ")
+floatt = input("Ingrese un numero con coma: ")
+
+print( type(texto) )
+print( type(boolean) )
+print( type(integer) )
+print( type(floatt) )
